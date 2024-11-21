2687ea6ed92552da2b74dab1b9aeabd96c5cce75
231043 <nabeken32h@gmail.com>
2024-11-21 10:42:07 +0900

    キャラクターとアニメーションの追加

--- My project/Assets/Script/MovingCharact.cs
+++ My project/Assets/Script/MovingCharact.cs
@@ -5,49 +5,74 @@ using UnityEngine;
 public class MovingCharact : MonoBehaviour
 {
     Rigidbody rb;
+    Animator anim;
+    Transform _transform;
+    float _horizontal;
+    float _vertical;
     bool MovuFlagForward = false;
     bool MovuFlagRight = false;
     bool MovuFlagLeft = false;
     bool MovuFlagBack = false;
 
+    Quaternion _playerRotation;
+    Vector3 _aim;
     // Start is called before the first frame update
     void Start()
     {
         // フレームレートを400psに設定する
         Application.targetFrameRate = 400;
         rb = GetComponent<Rigidbody>(); //rigidbodyを取得
+        anim = GetComponent<Animator>();
+        _transform = GetComponent<Transform>();
+        _playerRotation = _transform.rotation;
     }
 
     void FixedUpdate()
     {
-        //wキー(前方移動)
-        if(MovuFlagForward)
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        var _horizontalRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+
+        _aim = _horizontalRotation * new Vector3(_horizontal, 0, _vertical).normalized;
+
+        if (_aim.magnitude > 0.5f)
         {
-            Vector3 normal = new Vector3(0f,0f,7f);
+            _playerRotation = Quaternion.LookRotation(_aim, Vector3.up);
+        }
+
+        _transform.rotation = Quaternion.RotateTowards(_transform.rotation, _playerRotation, 600 * Time.deltaTime); // Quaternion.RotateTowards(回転前の向き, 回転後の向き, 回転する速さ);
+
+
+        //wキー(前方移動)
+        if (MovuFlagForward)
+        {
+            Vector3 normal = new Vector3(0f, 0f, 7f);
             rb.AddForce(normal);
             MovuFlagForward = false;
         }
         // Sキー（後方移動）
-        if(MovuFlagBack)
+        if (MovuFlagBack)
         {
-            Vector3 normal = new Vector3(0f,0f,-7f);
+            Vector3 normal = new Vector3(0f, 0f, -7f);
             rb.AddForce(normal);
             MovuFlagBack = false;
         }
         // Dキー（右移動）
-        if(MovuFlagRight)
+        if (MovuFlagRight)
         {
-            Vector3 normal = new Vector3(7f,0f,0f);
+            Vector3 normal = new Vector3(7f, 0f, 0f);
             rb.AddForce(normal);
             MovuFlagRight = false;
         }
         // Aキー（左移動）
-        if(MovuFlagLeft)
+        if (MovuFlagLeft)
         {
-            Vector3 normal = new Vector3(-7f,0f,0f);
+            Vector3 normal = new Vector3(-7f, 0f, 0f);
             rb.AddForce(normal);
             MovuFlagLeft = false;
         }
+
 
     }
 
@@ -55,26 +80,37 @@ public class MovingCharact : MonoBehaviour
     void Update()
     {
         //wキー(前方移動)
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             MovuFlagForward = true;
+
         }
+
         // Sキー（後方移動）
         if (Input.GetKey(KeyCode.S))
         {
             MovuFlagBack = true;
         }
- 
+
         // Dキー（右移動）
         if (Input.GetKey(KeyCode.D))
         {
             MovuFlagRight = true;
         }
- 
+
         // Aキー（左移動）
         if (Input.GetKey(KeyCode.A))
         {
             MovuFlagLeft = true;
+        }
+
+        if (MovuFlagBack || MovuFlagForward || MovuFlagLeft || MovuFlagRight)//移動キーを押したら
+        {
+            anim.SetBool("walking", true);//歩くアニメーション
+        }
+        else
+        {
+            anim.SetBool("walking", false);//何も押されなかったら止まる
         }
     }
 }
