3c23c2fb6d3c9fd041ec322e7ee98a7fc3d51104
Kameiyuto^ <kamei.11y17@gmail.com>
2025-02-20 13:36:32 +0900

    敵を倒して”討伐完了！！”が出る

--- My project/Assets/Script/ChangeCharacter.cs
+++ My project/Assets/Script/ChangeCharacter.cs
@@ -71,7 +71,7 @@ public class ChangeCharacter : MonoBehaviour //リストでバトルに行って
     // Update is called once per frame
     void Update()
     {
-        
+        NowPlayer.name = "Player";
     }
 
     public void PlayerNameAdd(string playername)
--- My project/Assets/Script/DefeatTextManager.cs
+++ My project/Assets/Script/DefeatTextManager.cs
@@ -1,46 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class DefeatTextManager : MonoBehaviour
 {
-    public GameObject defeatText;
-    public GameObject player;
-    private bool isTextActive = false;
-    // Start is called before the first frame update
+    public GameObject defeatText; // 討伐完了テキスト
+    public Text textComponent;
+    private bool isTextActive = false; // テキストが表示中か判定
+    private bool canClose = false; //拡大完了するまでクリック無効
+
     void Start()
     {
-        defeatText.SetActive(false);
+        defeatText.SetActive(false); // 最初は非表示
     }
 
-    //討伐完了テキストを表示
+    // 討伐完了テキストを表示して全体を停止
     public void ShowDefeatText()
     {
         defeatText.SetActive(true);
         isTextActive = true;
+        canClose = false;
+        Time.timeScale = 0; // **ゲーム全体を停止**
 
-        //プレイヤの操作を無効か
-        if(player != null)
-        {
-            player.GetComponent<chara>().enabled = false;
-        }
+        StartCoroutine(ScaleTextEffect()); //テキスト拡大アニメーション
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //クリックでテキストを消してプレイヤーの操作を有効か
-        if(isTextActive && Input.GetMouseButtonDown(0))
+        // クリックでテキストを消してゲームを再開
+        if (isTextActive && canClose && Input.GetMouseButtonDown(0))
         {
             defeatText.SetActive(false);
             isTextActive = false;
-
-            //プレイヤの操作を最有効化
-            if(player != null)
-            {
-                player.GetComponent<chara>().enabled = true;
-            }
+            Time.timeScale = 1; // **ゲーム再開**
         }
+    }
+    IEnumerator ScaleTextEffect()
+    {
+        float duration = 2.0f; //拡大にかかる時間
+        float elapsed = 0f;
+        Vector3 startScale = Vector3.one * 1.2f; //初期サイズ
+        Vector3 endScale = Vector3.one * 5.0f; //最終サイズ
+
+        textComponent.transform.localScale = startScale;
+
+        while(elapsed < duration)
+        {
+            elapsed += Time.unscaledDeltaTime; //停止中でも時間をカウント
+            float t = elapsed / duration;
+            textComponent.transform.localScale = Vector3.Lerp(startScale, endScale,t);
+            yield return null;
+        }
+        canClose = true;
     }
 }
